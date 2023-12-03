3e049bbba45c3e3f5bd631f8bd9ca38151f19ace
IMeeus <maeleeuxs@gmail.com>
2023-12-04 00:03:09 +0100

    start D12P1

--- D11P2/Program.cs
+++ D11P2/Program.cs
@@ -1,2 +1,99 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+var monkeys = File
+    .ReadAllText("input.txt")
+    .Split(Environment.NewLine + Environment.NewLine);
+
+Match match;
+List<int> totalInspects = [0, 0, 0, 0, 0, 0, 0, 0];
+List<long>[] monkeyBags = [[], [], [], [], [], [], [], []];
+for (int i = 0; i < 10_0000; i++)
+{
+    foreach (var monkey in monkeys)
+    {
+        var lines = monkey.Split(Environment.NewLine);
+
+        match = Regex.Match(monkey, @"Monkey (\d+)");
+        int monkeyId = int.Parse(match.Groups[1].Value);
+
+        if (i == 0)
+        {
+            monkeyBags[monkeyId] = lines[1]
+            .Split(':')[1]
+            .Split(',')
+            .Select(x => long.Parse(x))
+            .Concat(monkeyBags[monkeyId])
+            .ToList();
+        }
+
+        var items = monkeyBags[monkeyId].ToList();
+
+        foreach (var item in items)
+        {
+            long x = item;
+
+            // inspect
+
+            match = Regex.Match(monkey, @"Operation: new = (\S+) (.) (\S+)");
+
+            var o1 = match.Groups[1].Value == "old"
+                ? x
+                : int.Parse(match.Groups[1].Value);
+
+            var o2 = match.Groups[3].Value == "old"
+                ? x
+                : int.Parse(match.Groups[3].Value);
+
+            var opp = match.Groups[2].Value;
+
+            if (opp == "+")
+            {
+                x = o1 + o2;
+            }
+            else
+            {
+                x = o1 * o2;
+            }
+
+            totalInspects[monkeyId]++;
+
+            // relief
+
+            var mod = (17 * 13 * 19 * 7 * 11 * 3 * 2 * 5);
+            if (x % mod == 0)
+            {
+                x /= mod;
+            }
+
+            // test
+
+            var div = int.Parse(Regex
+                .Match(monkey, @"Test: divisible by (\d+)")
+                .Groups[1]
+                .Value);
+
+            var mTrue = int.Parse(Regex.Match(monkey, @"If true: throw to monkey (\d+)").Groups[1].Value);
+            var mFalse = int.Parse(Regex.Match(monkey, @"If false: throw to monkey (\d+)").Groups[1].Value);
+
+            // throw
+
+            if (x % div == 0)
+            {
+                monkeyBags[mTrue].Add((long)x);
+            }
+            else
+            {
+                monkeyBags[mFalse].Add((long)x);
+            }
+        }
+
+        monkeyBags[monkeyId].Clear();
+    }
+}
+
+totalInspects.Sort();
+
+var m1 = totalInspects[7];
+var m2 = totalInspects[6];
+
+Console.WriteLine(m1 * m2);
---
+++ D12P1/Program.cs
@@ -0,0 +1,64 @@
+﻿var lines = File.ReadAllLines("input.txt");
+
+List<Node> _spots = [];
+for (int y = 0; y < lines.Length; y++)
+{
+    for (int x = 0; x < lines[y].Length; x++)
+    {
+        _spots.Add(new(x, y, lines[y][x]));
+    }
+}
+
+int shortestPath = int.MaxValue;
+Dictionary<(int x, int y), int> _visited = [];
+var root = _spots.Single(spot => spot.c == 'S');
+root.c = 'a';
+var destination = _spots.Single(spot => spot.c == 'E');
+destination.c = 'z';
+
+void Search(Node node, int distance)
+{
+    _visited.Add((node.x, node.y), distance);
+
+    List<Node> candidates = [
+        _spots.SingleOrDefault(spot => spot.x == node.x - 1 && spot.y == node.y), // left
+        _spots.SingleOrDefault(spot => spot.x == node.x + 1 && spot.y == node.y), // right
+        _spots.SingleOrDefault(spot => spot.x == node.x && spot.y == node.y - 1), // up
+        _spots.SingleOrDefault(spot => spot.x == node.x && spot.y == node.y + 1), // down
+    ];
+
+    foreach (var candidate in candidates)
+    {
+        if (candidate is null) continue;
+        if (candidate.c > node.c + 1) continue;
+        if (candidate == destination)
+        {
+            if (distance < shortestPath)
+            {
+                shortestPath = distance + 1;
+            }
+            continue;
+        }
+        if (_visited.ContainsKey((candidate.x, candidate.y))) continue;
+
+        Search(candidate, distance + 1);
+    }
+}
+
+Search(root, 0);
+
+Console.WriteLine(shortestPath);
+
+internal class Node
+{
+    public int x { get; set; }
+    public int y { get; set; }
+    public char c { get; set; }
+
+    public Node(int x, int y, char c)
+    {
+        this.x = x;
+        this.y = y;
+        this.c = c;
+    }
+}
